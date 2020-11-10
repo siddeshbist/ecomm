97b6ed0237415a55a95f78e6789904ab1559fdeb
Siddesh <siddesh@Siddeshs-MacBook-Pro.local>
2020-11-10 12:08:45 -0500

    password error display

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -4,7 +4,12 @@ const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
 //destructuring requireEmail object
-const {requireEmail,requirePassword,requirePasswordConfirmation} = require('./validators');
+const {requireEmail,
+        requirePassword,
+        requirePasswordConfirmation,
+        requireEmailExists,
+        requireValidPasswordForUser
+        } = require('./validators');
 
 const router = express.Router();
 
@@ -50,39 +55,16 @@ router.get('/signin',(req,res) =>{
     res.send(signinTemplate());
 });
 
-router.post('/signin',[
-    check('email')
-    .trim().normalizeEmail().isEmail().withMessage('Must provide a valid email')
-    .custom(async (email) => {
-        const user = await usersRepo.getOneBy({email:email})
-        if(!user){
-            throw new Error("email not found");
-        }
-    }),
-    check('password')
-        .trim()
-        .custom(async (password, {req})=>{
-            const user = await usersRepo.getOneBy({email: req.body.email})
-            if(!user){
-                throw new Error('Invalid Password');
-            }
-            const validPassword = await usersRepo.comparePassword(
-                user.password,
-                password
-            );
-            if(!validPassword){
-                throw new Error('Invalid Password');
-            }
-        })
-],async (req,res)=>{
+router.post('/signin',[ requireEmailExists,requireValidPasswordForUser ],async (req,res)=>{
     const errors = validationResult(req);
+    console.log(errors)
     const{email} = req.body;
     const user = await usersRepo.getOneBy({email});
 
     req.session.userId = user.id;
 
     res.send('You are signed in');
-});
+})
 
 //export router to be used with other files in project
 
