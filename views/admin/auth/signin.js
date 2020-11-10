6f94726a3dc8404b8f03dd80c2031524698fef24
Siddesh <siddesh@Siddeshs-MacBook-Pro.local>
2020-11-10 12:15:24 -0500

    template helpter function

--- views/admin/auth/signin.js
+++ views/admin/auth/signin.js
@@ -1,10 +1,21 @@
 const layout = require('../layout');
 
-module.exports = () => {
+
+const getError = (errors,prop) => {
+    try{
+        return errors.mapped()[prop].msg
+    } catch(err){
+        return '';
+    }
+};
+
+module.exports = ({errors}) => {
     return layout({content:`<div>
     <form method='POST'>
         <input name="email" placeholder="email"/>
+        ${getError(errors,'email')}
         <input name="password" placeholder="password"/>
+        ${getError(errors,'password')}
         <button>Sign In</button>
     </form>
 </div>
