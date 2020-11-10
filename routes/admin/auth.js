507b12ed4f05863ef374d78b90579922baac55d4
Siddesh <siddesh@Siddeshs-MacBook-Pro.local>
2020-11-10 10:50:49 -0500

    added validators and error messages

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {check} = require('express-validator');
+const {check,validationResult} = require('express-validator');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
@@ -10,7 +10,32 @@ router.get('/signup',(req,res) => {
     res.send(signupTemplate({req}));
 });
 
-router.post('/signup',[check('email'),check('password'),check('passwordConfirmation')],async(req,res)=>{
+router.post('/signup',[
+    check('email')
+    .trim()
+    .normalizeEmail()
+    .isEmail()
+    .withMessage("Must be a valid email")
+    .custom(async (email) =>{
+        const existingUser = await usersRepo.getOneBy({email});
+        if(existingUser){
+            throw new Error('email in use')
+        }
+    }),
+    check('password').trim().isLength({min:4,max:20}).withMessage('Must be between 4 and 20 characters'),
+    check('passwordConfirmation')
+    .trim()
+    .isLength({min:4,max:20})
+    .withMessage('Must be between 4 and 20 characters')
+    //{req} contains password
+    .custom((passwordConfirmation,{req}) => {
+        if(passwordConfirmation !== req.body.password){
+            throw new Error('Passwords must match')
+        }
+    })],
+    async(req,res)=>{
+        const errors = validationResult(req);
+        console.log(errors)
     //see contents of form data is in the incoming request
     // req.on('data',data =>{
     //     const parsed = data.toString('utf8').split('&');
@@ -23,14 +48,9 @@ router.post('/signup',[check('email'),check('password'),check('passwordConfirmat
     // });
     const {email,password,passwordConfirmation} = req.body;
     //to check if email aleady exists
-    const existingUser = await usersRepo.getOneBy({email});
-    if(existingUser){
-        return res.send('Email is use');
-    }
 
-    if (password !== passwordConfirmation){
-        return res.send("Passwords much match")
-    }
+
+
 
     //Create a user in our user repo to represent this person
     const user = await usersRepo.create({email,password});
