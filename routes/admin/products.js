d01ca384509c6ec7b66e78447fe23e3833bd739e
Siddesh <siddesh@Siddeshs-MacBook-Pro.local>
2020-12-23 21:51:38 -0500

    added multer library for image upload

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const {validationResult} = require('express-validator')
+const multer = require('multer');
+
+//authored by us
 const productsRepo = require('../../repositories/products')
 const productsNewTemplate = require('../../views/admin/products/new');
 const {requireTitle, requirePrice} = require('./validators')
+
+
 const router = express.Router();
+const upload = multer({storage: multer.memoryStorage()});
 
 router.get('/admin/products',(req,res) => {
 
@@ -13,9 +19,9 @@ router.get('/admin/products/new',(req,res) => {
     res.send(productsNewTemplate({}));
 });
 
-router.post('/admin/products/new',[requireTitle,requirePrice],(req,res) =>{
+router.post('/admin/products/new',[requireTitle,requirePrice],upload.single('image'),(req,res) =>{
     const errors = validationResult(req);
-    console.log(errors);
+    console.log(req.file);
 
     res.send('submitted');
 });
